6f553c9aff18928136c9ce8f8b77e0eec5de82d7
nolan-the-first <125075091+nolan-the-first@users.noreply.github.com>
2023-02-10 20:12:30 +0300

    Update SendData.ts

--- Services/SendData.ts
+++ Services/SendData.ts
@@ -2,5 +2,7 @@ import { error } from "console";
 export const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export async function sendData(firstId, secondId) {
+  console.log("Id from Column 1 is: " + firstId);
+  console.log("Id from Column 2 is: " + secondId);
   await delay(2500);
 }
